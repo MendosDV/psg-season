890d74c9abc59e3499370326703fba3c4d0cc62e
Mickael Riss <mendos.prod@gmail.com>
2023-07-14 20:09:12 +0200

    players done

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,18 @@
 class PlayersController < ApplicationController
   def index
-    @players = Player.where(position: params[:position])
+    @players = Player.where(position: "goalkeeper")
+    if params[:query].present?
+      @players = Player.all
+      sql_subquery = <<~SQL
+        players.position ILIKE :query
+      SQL
+      @players = @players.where(sql_subquery, query: "%#{params[:query]}%")
+    end
+  end
+
+  private
+
+  def params_player
+    params.require(:player).permit(:first_name, :last_name, :position)
   end
 end
--- app/javascript/controllers/players_controller.js
+++ app/javascript/controllers/players_controller.js
@@ -1,20 +1,24 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {
-  static targets = ["players"];
+// export default class extends Controller {
+//   static targets = ["players"];
 
-  showPlayer(event) {
-    const position = event.target.dataset.position;
-    const url = `/players?position=${position}`;
+//   showPlayer(event) {
+//     const position = event.target.dataset.position;
+//     console.log(position);
+//     const url = `/players?position=${position}`;
 
-    fetch(url)
-      .then(response => response.text())
-      .then(html => {
-        const playersContainer = this.playersTarget;
-        playersContainer.innerHTML = html;
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-}
+//     fetch(url)
+//       .then(response => response.text())
+//       .then(html => {
+//         const parser = new DOMParser();
+//         const doc = parser.parseFromString(html, "text/html");
+//         const playerInfo = doc.querySelector(".player-info");
+//         const playersContainer = this.playersTarget;
+//         playersContainer.innerHTML = playerInfo.innerHTML;
+//       })
+//       // .catch(error => {
+//       //   console.error('Error:', error);
+//       // });
+//   }
+// }
