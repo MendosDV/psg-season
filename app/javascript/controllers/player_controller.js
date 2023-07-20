e7db8f67404f062cabf8c6cdcc277482e3bdb61d
Mickael Riss <mendos.prod@gmail.com>
2023-07-20 14:38:55 +0200

    Cards done

--- app/javascript/controllers/player_controller.js
+++ app/javascript/controllers/player_controller.js
@@ -4,10 +4,6 @@ export default class extends Controller {
   static targets = ["playerCard"];
 
   hello(event) {
-    // const playerFirstName = event.target.dataset.firstName;
-    // const playerLastName = event.target.dataset.lastName;
-    // console.log(`Hello ${playerFirstName} ${playerLastName}`);
-
     const playerId = event.target.dataset.id;
     console.log("ID :", playerId);
 
